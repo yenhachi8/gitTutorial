be27a5ab30a13320600f9ddab496c761c57c98db
yen <yenhachi8@gmail.com>
2022-02-06 10:16:25 +0800

    viewcontroller

--- gitTutorial/ViewController.swift
+++ gitTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hello")
         print("commit")
+        print("viewcontroller")
     }
 
 
